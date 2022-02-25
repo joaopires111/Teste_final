7ae9d73f2a81732185a96cdfcfbf4cece20ebb18
João Pires <61878156+joaopires111@users.noreply.github.com>
2022-02-25 23:28:31 +0000

    branch2

--- app/src/main/java/ipvc/estg/teste/MainActivity.kt
+++ app/src/main/java/ipvc/estg/teste/MainActivity.kt
@@ -2,11 +2,21 @@ package ipvc.estg.teste
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("", "")
     }
 
     //suspeito
